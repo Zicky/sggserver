b5eca62598c3c8ba6ff3dd796327c9101861dd64
Zicky <sarah.huangzq@gmail.com>
2012-02-24 15:47:54 -0500

    'Modified setting to sgg'

--- settings.py
+++ settings.py
@@ -11,10 +11,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', #'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': './dblocsql',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', #'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sgg',                      # Or path to database file if using sqlite3.
+        'USER': 'huangzi',                      # Not used with sqlite3.
+        'PASSWORD': 'huangzi@sgg@osu.edu',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
